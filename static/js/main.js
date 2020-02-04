f280487fa0c1f385a31a7ed3e68b91a2660d6518
Shithij <raishithij1999@gmail.com>
2020-02-04 19:04:48 +0530

    2019 to 2020 (;

--- static/js/main.js
+++ static/js/main.js
@@ -180,6 +180,11 @@
 
          navigation_links.parent().removeClass("current");
 			active_link.parent().addClass("current");
+			// if (element.target === '_blank') {
+            //     window.open(element.href);    
+            // } else {
+            //     location = element.href;
+            // }
 
 		},
 
@@ -190,20 +195,20 @@
 	/*---------------------------------------------------- */
   	/* Smooth Scrolling
   	------------------------------------------------------ */
-  	// $('.smoothscroll').on('click', function (e) {
-    //
-	 	// e.preventDefault();
-    //
-   	// var target = this.hash,
-    // 	$target = $(target);
-    //
-    // 	$('html, body').stop().animate({
-    //    	'scrollTop': $target.offset().top
-    //   }, 800, 'swing', function () {
-    //   	window.location.hash = target;
-    //   });
-    //
-  	// });
+  	$('.smoothscroll').on('click', function (e) {
+    
+	 	e.preventDefault();
+    
+   	var target = this.hash,
+    	$target = $(target);
+    
+    	$('html, body').stop().animate({
+       	'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+      	window.location.hash = target;
+      });
+    
+  	});
 
 
    /*---------------------------------------------------- */
